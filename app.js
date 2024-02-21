0f3c7ccf6f84b9f7180262bc35ae4470981362fa
agent-indigo <bradenhynes@gmail.com>
2024-02-21 10:12:45 -0330

    Make app.js immediately invocable

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import Config from "./app/Config.mjs"
 import Help from "./app/Help.mjs"
 import Init from "./app/Init.mjs"
 import Token from "./app/Token.mjs"
-const app = () => {
+const app = (() => {
     const args = process.argv.slice(2);
     const commands = {
         "-h": Help,
@@ -17,5 +17,5 @@ const app = () => {
     } else {
         console.log("Command not found");
     }
-}
+})()
 export default app
