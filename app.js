bc48982dff527fa38de531a161bde307e0406cf4
agent-indigo <bradenhynes@gmail.com>
2024-02-21 09:23:15 -0330

    Created modular function expression in app.js

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ import Config from "./app/Config.mjs"
 import Help from "./app/Help.mjs"
 import Init from "./app/Init.mjs"
 import Token from "./app/Token.mjs"
-
-const args = process.argv.slice(2);
-const commands = {
-    "-h": Help,
-    "--help": Help,
-    "config": Config,
-    "init": Init,
-    "token": Token,
+const app = () => {
+    const args = process.argv.slice(2);
+    const commands = {
+        "-h": Help,
+        "--help": Help,
+        "config": Config,
+        "init": Init,
+        "token": Token,
+    }
+    const command = args[0];
+    if (commands.hasOwnProperty(command)) {
+        commands[command](args.slice(1));
+    } else {
+        console.log("Command not found");
+    }
 }
-const command = args[0];
-if (commands.hasOwnProperty(command)) {
-    commands[command](args.slice(1));
-} else {
-    console.log("Command not found");
-}
+export default app
