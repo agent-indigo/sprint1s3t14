22e8491e10eec87b43a4241762402e9825e7483b
= <138686566+Brady-G@users.noreply.github.com>
2024-02-21 07:19:35 -0330

    Revert back moving of app contents

---
+++ app.js
@@ -0,0 +1,19 @@
+import Config from "./app/Config.mjs"
+import Help from "./app/Help.mjs"
+import Init from "./app/Init.mjs"
+import Token from "./app/Token.mjs"
+
+const args = process.argv.slice(2);
+const commands = {
+    "-h": Help,
+    "--help": Help,
+    "config": Config,
+    "init": Init,
+    "token": Token,
+}
+const command = args[0];
+if (commands.hasOwnProperty(command)) {
+    commands[command](args.slice(1));
+} else {
+    console.log("Command not found");
+}
