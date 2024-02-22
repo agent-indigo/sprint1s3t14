9a84d23151bc792b5edfa10253c06ad4175b8031
VK-Ampah <valentine.ampah@keyin.com>
2024-02-22 04:53:26 -0330

    update entity  DDL's  and clean up

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -1,4 +1,4 @@
--- Customers
+-- CREATING  CUSTOMERS
 CREATE TABLE IF NOT EXISTS customer (
 	 customer_id SERIAL PRIMARY KEY
 	, first_name VARCHAR (50) NOT NULL
@@ -6,18 +6,30 @@ CREATE TABLE IF NOT EXISTS customer (
 	, phone VARCHAR (20) NOT NULL
 	, email VARCHAR (50) 
 );
--- delivery Address
+-- CREATING delivery Address
 CREATE TABLE IF NOT EXISTS delivery_address (
-    delivery_address_id SERIAL PRIMARY KEY NOT NULL
-    , customer_id int NOT NULL
-    , address_type VARCHAR (50) NOT NULL
-    , city VARCHAR (50) NOT NULL
-    , province VARCHAR (50) NOT NULL
-    , street VARCHAR (50) NOT NULL
-    , postal_code CHAR (6) NOT NULL
-    , CONSTRAINT fkey_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id)	
+	delivery_address_id SERIAL PRIMARY KEY
+	, customer_id int
+	, address_type VARCHAR (50)
+	, city VARCHAR (50)
+	, province VARCHAR (50)
+	, street VARCHAR (50)
+	, postal_code CHAR (6)
+	, CONSTRAINT fkey_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id)	
 );
--- delivery details
+ALTER TABLE delivery_address
+ALTER COLUMN customer_id SET NOT NULL,
+ALTER COLUMN address_type SET NOT NULL,
+ALTER COLUMN city SET NOT NULL,
+ALTER COLUMN province SET NOT NULL,
+ALTER COLUMN street SET NOT NULL,
+ALTER COLUMN postal_code SET NOT NULL;
+
+ALTER TABLE delivery_address 
+ALTER COLUMN postal_code TYPE VARCHAR (20) ;
+
+
+-- DETLIVERY DETAILS
 CREATE TABLE IF NOT EXISTS delivery_details (
 	delivery_id serial PRIMARY KEY
 	, delivery_address_id INT NOT NULL
@@ -29,6 +41,9 @@ CREATE TABLE IF NOT EXISTS delivery_details (
 	, CONSTRAINT fkey_order FOREIGN KEY (order_id) REFERENCES orders(order_id)
 	, CONSTRAINT fkey_customer FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 	);
+ALTER TABLE delivery_details 
+ALTER COLUMN delivery_date TYPE TIMESTAMP;
+
 
 -- shopping cart
 CREATE TABLE IF NOT EXISTS shopping_cart (
@@ -36,7 +51,7 @@ CREATE TABLE IF NOT EXISTS shopping_cart (
     customer_id INT NOT NULL, 
     CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
-
+-- shopping cart items
 CREATE TABLE IF NOT EXISTS shopping_cart_items (
     cart_item_id SERIAL PRIMARY KEY
     ,cart_id INT NOT NULL
@@ -48,20 +63,24 @@ CREATE TABLE IF NOT EXISTS shopping_cart_items (
     ,CONSTRAINT chk_quantity_positive CHECK (quantity > 0)
     ,CONSTRAINT chk_price_positive CHECK (price > 0) 
 );
+ALTER TABLE shopping_cart_items 
+DROP COLUMN quantity;
 
--- orders table
+-- order table
 CREATE TABLE IF NOT EXISTS orders (
-    order_id serial PRIMARY KEY
-    , customer_id INT NOT NULL
-    , order_date DATE NOT NULL
-    , description VARCHAR (255) NOT NULL
-    , total_amount DECIMAL(10,2) NOT NULL
-    ,CONSTRAINT fkey_customer_id FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
+order_id serial PRIMARY KEY
+, customer_id INT NOT NULL
+, order_date DATE NOT NULL
+, description VARCHAR (255) NOT NULL
+, total_amount DECIMAL(10,2) NOT NULL
+,CONSTRAINT fkey_customer_id FOREIGN KEY (customer_id) REFERENCES customer(customer_id)
 );
-
+-- adding price check constraint to orders
 ALTER TABLE orders
 ADD CONSTRAINT chk_price_positive CHECK (total_amount > 0);
 
+ALTER TABLE orders 
+ALTER COLUMN order_date TYPE TIMESTAMP;
 
 
 --- order items
@@ -74,12 +93,15 @@ CREATE TABLE IF NOT EXISTS order_item (
     ,CONSTRAINT fkey_order FOREIGN KEY (order_id) REFERENCES orders(order_id)
     ,CONSTRAINT fkey_menu_item FOREIGN KEY (menu_item_id) REFERENCES menu_item(menu_item_id)
 );
--- adding orice check constraint to order items
+-- adding price check constraint to order items
 ALTER TABLE order_item
 ADD CONSTRAINT chk_price_positive CHECK (price > 0);
 ALTER TABLE order_item
 ADD CONSTRAINT chk_qty_positive CHECK (quantity > 0);
-
+ALTER TABLE order_item
+RENAME COLUMN price TO unit_price;
+ALTER TABLE order_item
+ADD COLUMN menu_details VARCHAR(255);
 
 -- promotion
 CREATE TABLE IF NOT EXISTS promotion (
@@ -89,29 +111,46 @@ CREATE TABLE IF NOT EXISTS promotion (
     ,end_date DATE NOT NULL
     ,CONSTRAINT check_discount_range CHECK (discount >= 0 AND discount <= 100)
 );
+-- alter promotion date field to timestamps
+ALTER TABLE promotion 
+ALTER COLUMN begin_date TYPE TIMESTAMP;
+ALTER TABLE promotion 
+ALTER COLUMN end_date TYPE TIMESTAMP;
+
+-- MENU CATEGORY
+CREATE TABLE IF NOT EXISTS menu_category (
+    category_id SERIAL PRIMARY KEY
+    ,name VARCHAR(50) NOT NULL
+    ,description VARCHAR(255) NOT NULL
+);
+
 -- menu
 CREATE TABLE IF NOT EXISTS menu_item (
-    menu_item_id serial PRIMARY KEY
-    , name VARCHAR (50) NOT NULL
-    , description VARCHAR (255) NOT NULL
-    , price DECIMAL(10,2) NOT NULL
-    , availability bool NOT NULL
-    , promo_code CHAR(6) NULL
-    , CONSTRAINT fkey_promotion FOREIGN KEY (promo_code) REFERENCES promotion(promo_code)
+menu_item_id serial PRIMARY KEY
+, name VARCHAR (50) NOT NULL
+, description VARCHAR (255) NOT NULL
+, price DECIMAL(10,2) NOT NULL
+, availability BOOL NOT NULL
+, promo_code CHAR(6) NULL
+, CONSTRAINT fkey_promotion FOREIGN KEY (promo_code) REFERENCES promotion(promo_code)
 );
 -- adding orice check constraint to menu items
 ALTER TABLE menu_item
 ADD CONSTRAINT chk_price_positive CHECK (price > 0);
 
--- ingredients
+-- add a foreign key column ot the menu_item entity and add column constraint
+ALTER TABLE menu_item
+ADD COLUMN category_id INT NOT NULL,
+ADD CONSTRAINT fkey_category FOREIGN KEY (category_id) REFERENCES menu_category(category_id); 
 
+-- ingredients
 CREATE TABLE IF NOT EXISTS ingredients (
     ingredients_id SERIAL PRIMARY KEY
     ,name VARCHAR(50) NOT NULL
     ,total_quantity INT NOT NULL
-    ,available_quantity INT NOT NULL
+    ,available_quantity INT NOT NULL 
     ,unit VARCHAR(20) NOT NULL
-    ,cost_per_unit DECIMAL(10, 2) NOT NULL,
+    ,cost_per_unit DECIMAL(10, 2) NOT NULL
     ,availability BOOL NOT NULL 
 );
 
--- database/employees.sql
+++ database/employees.sql
@@ -1,49 +1,21 @@
 -- Create combined employee/manager entity
 CREATE TABLE IF NOT EXISTS employee (
-    employee_id SERIAL PRIMARY KEY,
-    manager_id INT, -- Foreign key referencing employee_id (self-referencing)
-    address_id INT NOT NULL, -- Foreign key referencing address_id in employee_address table
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    role VARCHAR(50) NOT NULL,
-    hire_date TIMESTAMP NOT NULL,
-    salary DECIMAL(10, 2),
-    employment_status VARCHAR(20) NOT NULL, -- Added employment status field
-    termination_date DATE, -- Optional: Date of termination
-    termination_reason VARCHAR(255), -- Optional: Reason for termination
-    street VARCHAR(50) NOT NULL,
-    city VARCHAR(50) NOT NULL,
-    province VARCHAR(50) NOT NULL,
-    postal_code VARCHAR(20) NOT NULL
+    employee_id SERIAL PRIMARY KEY
+    ,manager_id INT -- Foreign key referencing employee_id (self-referencing)
+    ,address_id INT NOT NULL -- Foreign key referencing address_id in employee_address table
+    ,first_name VARCHAR(50) NOT NULL
+    ,last_name VARCHAR(50) NOT NULL
+    ,role VARCHAR(50) NOT NULL
+    ,hire_date TIMESTAMP NOT NULL
+    ,salary DECIMAL(10, 2)
+    ,employment_status VARCHAR(20) NOT NULL -- Added employment status field
+    ,termination_date DATE -- Optional: Date of termination
+    ,termination_reason VARCHAR(255) -- Optional: Reason for termination
+    ,street VARCHAR(50) NOT NULL
+    ,city VARCHAR(50) NOT NULL
+    ,province VARCHAR(50) NOT NULL
+    ,postal_code VARCHAR(20) NOT NULL
 );
 -- Add check constraint to ensure salary is positive
 ALTER TABLE employee
 ADD CONSTRAINT salary_check CHECK (salary > 0);
--- insert data into employee table
-INSERT INTO employee (manager_id, address_id, first_name, last_name, role, hire_date, salary, employment_status, termination_date, termination_reason, street, city, province, postal_code)
-VALUES
-    -- today
-    (NULL, 1, 'James', 'Doe', 'Manager', now(), 50000.00, 'active', NULL, NULL, '123 Main St', 'St. John''s', 'NL', 'A1B 2C3'),
-    (NULL, 2, 'Jimmy', 'Smith', 'Manager', now(), 55000.00, 'active', NULL, NULL, '456 Elm St', 'Mt. Pearl', 'NL', 'X1Y 2Z3'),
-    (3, 3, 'Albert', 'Johnson', 'Sales Associate', now(), 40000.00, 'active', NULL, NULL, '123 Main St', 'Paradise', 'NL', 'A1B 2C3'),
-    (4, 4, 'Bobby', 'Smitzer', 'Cashier', now(), 35000.00, 'active', NULL, NULL, '123 Main St', 'St. John''s', 'NL', 'A1B 2C3'),
-    (5, 5, 'Emmanuel', 'Doney', 'Sales Associate', now(), 38000.00, 'active', NULL, NULL, '456 Elm St', 'Paradise', 'NL', 'X1Y 2Z3'),
-    -- Last week
-
-    (NULL, 1, 'Jane', 'Doe', 'Manager', now() - INTERVAL '1 WEEK', 50000.00, 'active', NULL, NULL, '123 Main St', 'St. John''s', 'NL', 'A1B 2C3'),
-    (NULL, 2, 'Jane', 'Smith', 'Manager', now() - INTERVAL '1 WEEK', 55000.00, 'active', NULL, NULL, '456 Elm St', 'Mt. Pearl', 'NL', 'X1Y 2Z3'),
-    (3, 3, 'Alice', 'Johnson', 'Sales Associate', now() - INTERVAL '1 WEEK', 40000.00, 'active', NULL, NULL, '123 Main St', 'Paradise', 'NL', 'A1B 2C3'),
-    (4, 4, 'Bob', 'Smith', 'Cashier', now() - INTERVAL '1 WEEK', 35000.00, 'active', NULL, NULL, '123 Main St', 'St. John''s', 'NL', 'A1B 2C3'),
-    (5, 5, 'Emma', 'Davis', 'Sales Associate', now() - INTERVAL '1 WEEK', 38000.00, 'active', NULL, NULL, '456 Elm St', 'Paradise', 'NL', 'X1Y 2Z3'),
-    -- A month ago
-    (6, 6, 'Michael', 'Brown', 'Cashier', now() - INTERVAL '1 MONTH', 36000.00, 'active', NULL, NULL, '789 Oak St', 'Conception Bay St.', 'NL', 'Y3Z 4X1'),
-    (7, 7, 'David', 'Wilson', 'Cashier', now() - INTERVAL '1 MONTH', 35000.00, 'active', NULL, NULL, '890 Maple St', 'St. Anthony', 'NL', 'A1B 2C3'),
-    (8, 8, 'Jennifer', 'Clark', 'Sales Associate', now() - INTERVAL '1 MONTH', 42000.00, 'active', NULL, NULL, '567 Pine St', 'Mt. Pearl', 'NL', 'Y3Z 4X1'),
-    (9, 9, 'William', 'Martinez', 'Cashier', now() - INTERVAL '1 MONTH', 37000.00, 'active', NULL, NULL, '890 Maple St', 'Mt. Pearl', 'NL', 'A1B 2C3'),
-    (10, 10, 'Laura', 'Garcia', 'Store Standard Associate', now() - INTERVAL '1 MONTH', 38000.00, 'active', NULL, NULL, '123 Elm St', 'Goulds', 'NL', 'Y3Z 4X1'),
-    -- Yesterday
-    (NULL, 11, 'John', 'Smith', 'Manager', now() - INTERVAL '1 DAY', 55000.00, 'active', NULL, NULL, '456 Elm St', 'Mt. Pearl', 'NL', 'X1Y 2Z3'),
-    (12, 12, 'Emma', 'Johnson', 'Sales Associate', now() - INTERVAL '1 DAY', 40000.00, 'active', NULL, NULL, '123 Main St', 'Paradise', 'NL', 'A1B 2C3'),
-    (13, 13, 'Michael', 'Brown', 'Cashier', now() - INTERVAL '1 DAY', 36000.00, 'active', NULL, NULL, '789 Oak St', 'Conception Bay St.', 'NL', 'Y3Z 4X1'),
-    (14, 14, 'David', 'Wilson', 'Cashier', now() - INTERVAL '1 DAY', 35000.00, 'active', NULL, NULL, '890 Maple St', 'St. Anthony', 'NL', 'A1B 2C3'),
-    (15, 15, 'Jennifer', 'Clark', 'Sales Associate', now() - INTERVAL '1 DAY', 42000.00, 'active', NULL, NULL, '567 Pine St', 'Mt. Pearl', 'NL', 'Y3Z 4X1');
