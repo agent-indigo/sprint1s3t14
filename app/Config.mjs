abe1f3f3ac3c94a400a039626b5c0571b98349b5
agent-indigo <bradenhynes@gmail.com>
2024-02-21 22:32:28 -0330

    Add: Help & preliminary Token functionality

--- app/Config.mjs
+++ app/Config.mjs
@@ -1,16 +1,12 @@
+import Help from "./Help.mjs"
 const Config = args => {
-    const help = [
-        "Usage: node app.js config [options]",
-    
-        "Options:",
-        "  --help       Display this help message",
-    ]
     switch (args[0]) {
         case "-h":
         case "--help":
-            return console.log(help.join("\n"));
+            Help()
+            break
         default:
-            return console.log("Command not found");
+            return console.log("Command not found")
     }
 }
 export default Config
