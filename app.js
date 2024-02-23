88d09ab5806b1061259ef958ed73c91ddc18bbc1
Brady Green <138686566+Brady-G@users.noreply.github.com>
2024-02-23 14:07:31 -0330

    Handle errors more gracefully in CLI
    - Implement tryCatch function that handles errors
    - Move token methods to outer scope
    - Handle only specified types in token update and search
    - Remove self invoking function from app

--- app.js
+++ app.js
@@ -2,20 +2,18 @@ import Config from "./app/Config.mjs"
 import Help from "./app/Help.mjs"
 import Init from "./app/Init.mjs"
 import Token from "./app/Token.mjs"
-const app = (() => {
-    const args = process.argv.slice(2);
-    const commands = {
-        "-h": Help,
-        "--help": Help,
-        "config": Config,
-        "init": Init,
-        "token": Token,
-    }
-    const command = args[0];
-    if (commands.hasOwnProperty(command)) {
-        commands[command](args.slice(1));
-    } else {
-        console.log("Command not found");
-    }
-})()
-export default app
+
+const args = process.argv.slice(2);
+const commands = {
+    "-h": Help,
+    "--help": Help,
+    "config": Config,
+    "init": Init,
+    "token": Token,
+}
+const command = args[0];
+if (commands.hasOwnProperty(command)) {
+    commands[command](args.slice(1));
+} else {
+    console.log("Command not found");
+}
